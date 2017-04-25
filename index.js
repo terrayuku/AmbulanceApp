a3f35e1d2cfe40e38169ea8a077e37ecd10a5a49
terrayuku <peshyuku@gmail.com>
2017-04-25 21:12:50 +0200

    Home Page of the Patient Application

--- index.js
+++ index.js
@@ -2,55 +2,46 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+// import createLogger from 'redux-logger';
 import {AppContainer} from 'react-hot-loader';
 
-import weatherApp from './reducers';
-import App from './components/App';
-
-import './icons/css/weather-icons.css';
+// import weatherApp from './reducers'; // this is loaded when
+// creating a redux store.
+// to initialize the application
+import PatientApp from './components/PatientApp';
 
 import ons from 'onsenui';
 import 'onsenui/css/onsenui.css';
-import './stylus/index.styl';
+// import './stylus/index.styl';
 
-const logger = createLogger();
+// const logger = createLogger();
 
-const store = createStore(weatherApp,
-  window.devToolsExtension ? window.devToolsExtension() : f => f,
-  process.env.NODE_ENV === 'production'
-    ? applyMiddleware(thunk)
-    : applyMiddleware(thunk, logger)
-);
-
-// import {addLocationAndFetchWeather} from './actions';
-
-// // Loading all close by online car washers
-// [
-//   'Umtata',
-//   'Mount Frere',
-//   'Bhaziya'
-// ].forEach((city) => store.dispatch(addLocationAndFetchWeather(city)));
+// const store = createStore(weatherApp,
+// window.devToolsExtension ? window.devToolsExtension() : f => f,
+// process.env.NODE_ENV === 'production'
+// ? applyMiddleware(thunk)
+// : applyMiddleware(thunk, logger)
+// );
 
 const rootElement = document.getElementById('root');
 
 ons.ready(() => render(
   <AppContainer>
-    <Provider store={store}>
-      <App />
+    <Provider >
+      <PatientApp />
     </Provider>
   </AppContainer>,
   rootElement
 ));
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const NextApp = require('./components/App').default;
+  module.hot.accept('./components/PatientApp', () => {
+    const NextApp = require('./components/PatientApp').default;
     render(
       <AppContainer>
-        <Provider store={store}>
+        <Provider >
           <NextApp />
         </Provider>
       </AppContainer>,
